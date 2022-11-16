8eb2513dfb6894dc05b559c00e2582c2c150c188
lcharleux <ludovic.charleux@univ-smb.fr>
2022-11-16 22:42:37 +0000

    Late WIP

--- planification/course_makers/make_MECA654.py
+++ planification/course_makers/make_MECA654.py
@@ -1,164 +1,229 @@
 import json
 import copy
+import yaml
 
 # MECA654
 
 from automatic_university_scheduler.scheduling import Activity, Course
 
 course_label = "MECA654"
-course = Course(label=course_label, color="Green")
-teachers_acronyms = {
-    "EP": (["PAIREL ERIC"], 1),
-    "ER": (["ROUX EMILE"], 1),
-    "CE": (["ELMO KULANESAN CHRISTIAN"], 1),
-}
-
-exam_teachers = [teachers_acronyms[acr] for acr in ["EP", "ER", "CE"]]
 default_rooms = json.load(open("../default_rooms.json"))
-rooms = {
-    "amphi_x1": (["A-POLY-B120"], 1),
-    "big_info_rooms_x1": (["A-POLY-C213", "A-POLY-C214", "A-POLY-C215"], 1),
-    "info_rooms_x1": (
-        ["A-POLY-C213", "A-POLY-C214", "A-POLY-C215", "A-POLY-C216", "A-POLY-C217"],
-        1,
-    ),
+room_pools = {
+    "big_info_rooms": ["A-POLY-C213", "A-POLY-C214", "A-POLY-C215"],
+    "info_rooms": [
+        "A-POLY-C213",
+        "A-POLY-C214",
+        "A-POLY-C215",
+        "A-POLY-C216",
+        "A-POLY-C217",
+    ],
+    "amphi": ["A-POLY-B120"],
 }
-activities_ressources = {
-    "CM": {
-        1: [
-            ("MM-3", "ER", "amphi"),
-        ],
-        2: [
-            ("MM-3", "ER", "amphi"),
-        ],
-        3: [
-            ("MM-3", "ER", "amphi"),
-        ],
-        4: [
-            ("MM-3", "ER", "amphi"),
-        ],
-        5: [
-            ("MM-3", "ER", "amphi"),
-        ],
-        6: [
-            ("MM-3", "ER", "amphi"),
-        ],
-        7: [
-            ("MM-3", "ER", "amphi"),
-        ],
-    },
-    "TD": {
-        1: [
-            ("MM-3-A-TD", "CE", "default"),
-            ("MM-3-B-TD", "CE", "default"),
-            ("MM-3-C-TD", "CE", "default"),
-        ],
-        2: [
-            ("MM-3-A-TD", "CE", "default"),
-            ("MM-3-B-TD", "CE", "default"),
-            ("MM-3-C-TD", "AB", "default"),
-        ],
-        3: [
-            ("MM-3-A-TD", "CE", "default"),
-            ("MM-3-B-TD", "CE", "default"),
-            ("MM-3-C-TD", "AB", "default"),
-        ],
-        4: [
-            ("MM-3-A-TD", "CE", "default"),
-            ("MM-3-B-TD", "CE", "default"),
-            ("MM-3-C-TD", "AB", "default"),
-        ],
-        5: [
-            ("MM-3-A-TD", "CE", "default"),
-            ("MM-3-B-TD", "CE", "default"),
-            ("MM-3-C-TD", "AB", "default"),
-        ],
-        6: [
-            ("MM-3-A-TD", "CE", "default"),
-            ("MM-3-B-TD", "CE", "default"),
-            ("MM-3-C-TD", "AB", "default"),
-        ],
-        7: [
-            ("MM-3-A-TD", "CE", "default"),
-            ("MM-3-B-TD", "CE", "default"),
-            ("MM-3-C-TD", "AB", "default"),
-        ],
-        8: [
-            ("MM-3-A-TD", "CE", "default"),
-            ("MM-3-B-TD", "CE", "default"),
-            ("MM-3-C-TD", "AB", "default"),
-        ],
-        9: [
-            ("MM-3-A-TD", "CE", "default"),
-            ("MM-3-B-TD", "CE", "default"),
-            ("MM-3-C-TD", "AB", "default"),
-        ],
-        10: [
-            ("MM-3-A-TD", "CE", "big_info"),
-            ("MM-3-B-TD", "CE", "big_info"),
-            ("MM-3-C-TD", "AB", "big_info"),
-        ],
-    },
-    "TP": {
-        1: [
-            ("MM-3-A1", "CE", "info"),
-            ("MM-3-A2", "CE", "info"),
-            ("MM-3-B1", "CE", "info"),
-            ("MM-3-B2", "AB", "info"),
-            ("MM-3-C1", "AB", "info"),
-            ("MM-3-C2", "AB", "info"),
-        ],
-        2: [
-            ("MM-3-A1", "CE", "info"),
-            ("MM-3-A2", "CE", "info"),
-            ("MM-3-B1", "CE", "info"),
-            ("MM-3-B2", "AB", "info"),
-            ("MM-3-C1", "AB", "info"),
-            ("MM-3-C2", "AB", "info"),
-        ],
-        3: [
-            ("MM-3-A1", "CE", "info"),
-            ("MM-3-A2", "CE", "info"),
-            ("MM-3-B1", "CE", "info"),
-            ("MM-3-B2", "AB", "info"),
-            ("MM-3-C1", "AB", "info"),
-            ("MM-3-C2", "AB", "info"),
-        ],
-    },
-}
-
-
-def make_activities(activities_data, course):
-    all_activities = {}
-    for kind, activities_kind in activities_data.items():
-        all_activities[kind] = {}
-        for index, atomic_activities in activities_kind.items():
-            all_activities[kind][index] = []
-            activities =  all_activities[kind][index]  
-            for students, teachers, rooms in atomic_activities:
-                activity = Activity(
-                    label=f"{course_label}_{students}_{kind}_{index}",
-                    students=students,
-                    duration=6,
-                )
-                teacher = [teachers_acronyms[t] for t in teacher]
-                activity.add_ressources(kind="teachers", quantity=1, pool=teacher)
-                room_category = TD_rooms[index]
+data = yaml.safe_load(open(f"{course_label}_data/{course_label}.yml"))
+course = Course(label=course_label, color=data["color"])
+# ACTIVITIES
+activities = {}
+for label, activity in data["activities"].items():
+    students = activity["students"]
+    kind = activity["kind"]
+    duration = activity["duration"]
+    act = Activity(
+        label=f"{course_label}_{label}",
+        students=students,
+        duration=duration,
+    )
+    course.add_activity(act)
+    activities[label] = act
+    for ressource in ["rooms", "teachers"]:
+        pool_name = activity[ressource]["pool"]
+        value = activity[ressource]["value"]
+        if ressource == "rooms":
+            pool = []
+            for room_category in pool_name:
                 if room_category == "default":
-                    room_pool = default_rooms[students]
-                elif room_category == "info":
-                    room_pool = big_info_rooms
-                activity.add_ressources(kind="rooms", quantity=1, pool=room_pool)
-                if after != None:
-                    if after != None:
-                        activity.add_multiple_after(
-                            after, min_offset=min_offset, max_offset=max_offset
-                        )
-                course.add_activity(activity)
-                if add_to != None:
-                    add_to.add_activity(activity)
-                activities.append(activity)
-    return all_activities
+                    pool.append(default_rooms[students])
+                else:
+                    pool.append(room_pools[room_category])
+        if ressource == "teachers":
+            pool = [data["teachers_acronyms"][t] for t in pool_name]
+        act.add_ressources(kind=ressource, quantity=value, pool=pool) 
+
+# CONSTRAINTS
+activity_groups = data["activity_groups"]
+for constraint in data["constraints"]:
+    if constraint["kind"] == "succession":
+        start_group = constraint["start_after"]
+        end_group = constraint["activities"]
+        if "min_offset" in constraint.keys():
+            min_offset = constraint["min_offset"]
+        else:
+            min_offset = 0
+        if "max_offset" in constraint.keys():
+            max_offset = constraint["max_offset"]
+        else:
+            max_offset = None
+        for act in end_group:
+            pass
+
+
+
+
+#                     room_pool = default_rooms[students]
+#                 elif room_category == "info":
+#                     room_pool = big_info_rooms
+#                 activity.add_ressources(kind="rooms", quantity=1, pool=room_pool)
+# teachers_acronyms = {
+#     "EP": (["PAIREL ERIC"], 1),
+#     "ER": (["ROUX EMILE"], 1),
+#     "CE": (["ELMO KULANESAN CHRISTIAN"], 1),
+# }
+
+# exam_teachers = [teachers_acronyms[acr] for acr in ["EP", "ER", "CE"]]
+# default_rooms = json.load(open("../default_rooms.json"))
+# rooms = {
+#     "amphi_x1": (["A-POLY-B120"], 1),
+#     "big_info_rooms_x1": (["A-POLY-C213", "A-POLY-C214", "A-POLY-C215"], 1),
+#     "info_rooms_x1": (
+#         ["A-POLY-C213", "A-POLY-C214", "A-POLY-C215", "A-POLY-C216", "A-POLY-C217"],
+#         1,
+#     ),
+# }
+# activities_ressources = {
+#     "CM": {
+#         1: [
+#             ("MM-3", "ER", "amphi"),
+#         ],
+#         2: [
+#             ("MM-3", "ER", "amphi"),
+#         ],
+#         3: [
+#             ("MM-3", "ER", "amphi"),
+#         ],
+#         4: [
+#             ("MM-3", "ER", "amphi"),
+#         ],
+#         5: [
+#             ("MM-3", "ER", "amphi"),
+#         ],
+#         6: [
+#             ("MM-3", "ER", "amphi"),
+#         ],
+#         7: [
+#             ("MM-3", "ER", "amphi"),
+#         ],
+#     },
+#     "TD": {
+#         1: [
+#             ("MM-3-A-TD", "CE", "default"),
+#             ("MM-3-B-TD", "CE", "default"),
+#             ("MM-3-C-TD", "CE", "default"),
+#         ],
+#         2: [
+#             ("MM-3-A-TD", "CE", "default"),
+#             ("MM-3-B-TD", "CE", "default"),
+#             ("MM-3-C-TD", "AB", "default"),
+#         ],
+#         3: [
+#             ("MM-3-A-TD", "CE", "default"),
+#             ("MM-3-B-TD", "CE", "default"),
+#             ("MM-3-C-TD", "AB", "default"),
+#         ],
+#         4: [
+#             ("MM-3-A-TD", "CE", "default"),
+#             ("MM-3-B-TD", "CE", "default"),
+#             ("MM-3-C-TD", "AB", "default"),
+#         ],
+#         5: [
+#             ("MM-3-A-TD", "CE", "default"),
+#             ("MM-3-B-TD", "CE", "default"),
+#             ("MM-3-C-TD", "AB", "default"),
+#         ],
+#         6: [
+#             ("MM-3-A-TD", "CE", "default"),
+#             ("MM-3-B-TD", "CE", "default"),
+#             ("MM-3-C-TD", "AB", "default"),
+#         ],
+#         7: [
+#             ("MM-3-A-TD", "CE", "default"),
+#             ("MM-3-B-TD", "CE", "default"),
+#             ("MM-3-C-TD", "AB", "default"),
+#         ],
+#         8: [
+#             ("MM-3-A-TD", "CE", "default"),
+#             ("MM-3-B-TD", "CE", "default"),
+#             ("MM-3-C-TD", "AB", "default"),
+#         ],
+#         9: [
+#             ("MM-3-A-TD", "CE", "default"),
+#             ("MM-3-B-TD", "CE", "default"),
+#             ("MM-3-C-TD", "AB", "default"),
+#         ],
+#         10: [
+#             ("MM-3-A-TD", "CE", "big_info"),
+#             ("MM-3-B-TD", "CE", "big_info"),
+#             ("MM-3-C-TD", "AB", "big_info"),
+#         ],
+#     },
+#     "TP": {
+#         1: [
+#             ("MM-3-A1", "CE", "info"),
+#             ("MM-3-A2", "CE", "info"),
+#             ("MM-3-B1", "CE", "info"),
+#             ("MM-3-B2", "AB", "info"),
+#             ("MM-3-C1", "AB", "info"),
+#             ("MM-3-C2", "AB", "info"),
+#         ],
+#         2: [
+#             ("MM-3-A1", "CE", "info"),
+#             ("MM-3-A2", "CE", "info"),
+#             ("MM-3-B1", "CE", "info"),
+#             ("MM-3-B2", "AB", "info"),
+#             ("MM-3-C1", "AB", "info"),
+#             ("MM-3-C2", "AB", "info"),
+#         ],
+#         3: [
+#             ("MM-3-A1", "CE", "info"),
+#             ("MM-3-A2", "CE", "info"),
+#             ("MM-3-B1", "CE", "info"),
+#             ("MM-3-B2", "AB", "info"),
+#             ("MM-3-C1", "AB", "info"),
+#             ("MM-3-C2", "AB", "info"),
+#         ],
+#     },
+# }
+
+
+# def make_activities(activities_data, course):
+#     all_activities = {}
+#     for kind, activities_kind in activities_data.items():
+#         all_activities[kind] = {}
+#         for index, atomic_activities in activities_kind.items():
+#             all_activities[kind][index] = []
+#             activities =  all_activities[kind][index]
+#             for students, teachers, rooms in atomic_activities:
+#                 activity = Activity(
+#                     label=f"{course_label}_{students}_{kind}_{index}",
+#                     students=students,
+#                     duration=6,
+#                 )
+#                 teacher = [teachers_acronyms[t] for t in teacher]
+#                 activity.add_ressources(kind="teachers", quantity=1, pool=teacher)
+#                 room_category = TD_rooms[index]
+#                 if room_category == "default":
+#                     room_pool = default_rooms[students]
+#                 elif room_category == "info":
+#                     room_pool = big_info_rooms
+#                 activity.add_ressources(kind="rooms", quantity=1, pool=room_pool)
+#                 if after != None:
+#                     if after != None:
+#                         activity.add_multiple_after(
+#                             after, min_offset=min_offset, max_offset=max_offset
+#                         )
+#                 course.add_activity(activity)
+#                 if add_to != None:
+#                     add_to.add_activity(activity)
+#                 activities.append(activity)
+#     return all_activities
 
 
 # def make_CM(
@@ -352,6 +417,7 @@ def make_activities(activities_data, course):
 #     act.add_multiple_after(TP[3] + TD[6], min_offset=48)
 # for act in CC[2]:
 #     act.add_multiple_after(TD[12] + CC[1], min_offset=48)
+
 
 path = f"../activity_data/{course_label}.json"
 with open(path, "w") as f:
